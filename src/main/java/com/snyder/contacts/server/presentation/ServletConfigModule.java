348fce6bc86c9d994155c04c0d47cc4b6b1df8ac
SnyderGP <SnyderGP@gmail.com>
2013-08-27 15:28:00 -0700

    Adding ServletConfigModule

---
+++ src/main/java/com/snyder/contacts/server/presentation/ServletConfigModule.java
@@ -0,0 +1,16 @@
+package com.snyder.contacts.server.presentation;
+
+import com.google.inject.servlet.ServletModule;
+
+
+public class ServletConfigModule extends ServletModule
+{
+
+    @Override
+    protected void configureServlets()
+    {
+        serve("/contact/*").with(ContactServlet.class);
+        serve("/contact-summary/*").with(ContactSummaryServlet.class);
+    }
+    
+}
