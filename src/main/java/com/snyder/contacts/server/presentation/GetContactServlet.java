a6c1bebbfb289a33d8c01651283d7e162b820600
SnyderGP <SnyderGP@gmail.com>
2013-08-27 15:25:01 -0700

    Merging create/update servlet functionality into GetContactServlet

--- src/main/java/com/snyder/contacts/server/presentation/GetContactServlet.java
+++ src/main/java/com/snyder/contacts/server/presentation/GetContactServlet.java
@@ -7,10 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.common.net.MediaType;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.google.inject.Inject;
 import com.snyder.contacts.server.domain.ContactsDomain;
+import com.snyder.contacts.server.exceptions.InvalidContactException;
+import com.snyder.contacts.shared.model.Business;
 import com.snyder.contacts.shared.model.Contact;
+import com.snyder.contacts.shared.model.ContactType;
+import com.snyder.contacts.shared.model.Person;
 
 
 public class GetContactServlet extends HttpServlet
@@ -41,6 +47,61 @@ public class GetContactServlet extends HttpServlet
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.setContentLength(contactJson.length());
         resp.getWriter().append(contactJson);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
+        IOException
+    {
+        String contactJson = req.getParameter("contact");
+        ContactType type = ContactType.fromCode("contactTypeCode");
+        
+        Contact contact;
+        try
+        {
+            switch(type)
+            {
+                case PERSON:
+                    contact = GSON.fromJson(contactJson, Person.class);
+                    break;
+                case BUSINESS:
+                    contact = GSON.fromJson(contactJson, Business.class);
+                    break;
+                default:
+                    throw new Exception("Unknown contactTypeCode parameter");
+            }
+        }
+        catch (Exception e)
+        {
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.setContentType(MediaType.JSON_UTF_8.subtype());
+            resp.getWriter().append("[" + e.getMessage() + "]"); // errors return JSON string array
+            return;
+        }
+        
+        
+        try
+        {
+            if(contact.getId() < 0)
+            {
+                int id = domain.createContact(contact);
+                resp.getWriter().append(Integer.toString(id));
+            }
+            else
+            {
+                domain.updateContact(contact);
+            }
+            resp.setStatus(HttpServletResponse.SC_OK);
+        }
+        catch (InvalidContactException e)
+        {
+            String errorsJson = GSON.toJson(e.getErrors(), TypeToken.get(String.class).getType());
+            
+            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            resp.setContentLength(errorsJson.length());
+            resp.setContentType(MediaType.JSON_UTF_8.subtype());
+            resp.getWriter().append(errorsJson);
+        }
     }
     
 }
